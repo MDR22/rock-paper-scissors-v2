b5204dcc8938c95c4c1ba00340ae76ed403253f7
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2022-12-14 20:27:34 -0600

    Add function to play round to determine winner

--- game.js
+++ game.js
@@ -1,8 +1,19 @@
 //Create function for computer to randomly return rock, paper, or scissors
-function getComputerChoice () {
+function getComputerChoice() {
     const computerChoiceArray = ["rock",
         "paper",
         "scissors"];
     const computerChoiceSelector = computerChoiceArray[Math.floor(Math.random() * computerChoiceArray.length)];
     return computerChoiceSelector;
+}
+
+//Play round of rock-paper-scissors, compare random computer choice against player choice to determine winner
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return `It's a tie - you both chose ${playerSelection}`;
+    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
+        return `You win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}`;
+    }
 }

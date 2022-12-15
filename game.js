5f351c6ed2cdc37aeabb300f1af17c8ffa80526f
Mark Riegel <116507222+MDR22@users.noreply.github.com>
2022-12-14 20:30:13 -0600

    Convert user choice to lower case

--- game.js
+++ game.js
@@ -9,11 +9,12 @@ function getComputerChoice() {
 
 //Play round of rock-paper-scissors, compare random computer choice against player choice to determine winner
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
-        return `It's a tie - you both chose ${playerSelection}`;
-    } else if (playerSelection === "rock" && computerSelection === "scissors" || playerSelection === "paper" && computerSelection === "rock" || playerSelection === "scissors" && computerSelection === "paper") {
-        return `You win! ${playerSelection} beats ${computerSelection}`;
+    const playerSelectionLowerCase = playerSelection.toLowerCase();
+    if (playerSelectionLowerCase === computerSelection) {
+        return `It's a tie - you both chose ${playerSelectionLowerCase}`;
+    } else if (playerSelectionLowerCase === "rock" && computerSelection === "scissors" || playerSelectionLowerCase === "paper" && computerSelection === "rock" || playerSelectionLowerCase === "scissors" && computerSelection === "paper") {
+        return `You win - ${playerSelectionLowerCase} beats ${computerSelection}`;
     } else {
-        return `You lose! ${computerSelection} beats ${playerSelection}`;
+        return `You lose - ${computerSelection} beats ${playerSelectionLowerCase}`;
     }
 }
